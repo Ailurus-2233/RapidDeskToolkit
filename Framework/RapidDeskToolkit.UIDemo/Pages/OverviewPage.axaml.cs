a3d2230fb784b471c6350a44c046a5f24410617d
WangZhiYu <ailuru2233@outlook.com>
2025-03-27 17:38:22 +0800

    修改 UIDemo 中 Overview 的内容

--- Framework/RapidDeskToolkit.UIDemo/Pages/OverviewPage.axaml.cs
+++ Framework/RapidDeskToolkit.UIDemo/Pages/OverviewPage.axaml.cs
@@ -8,10 +8,15 @@ public partial class OverviewPage : UserControl, IPage
 {
     public OverviewPage()
     {
+        DataContext = this;
         InitializeComponent();
     }
 
     public string Title => Language.OverviewPageName;
 
     public UserControl GetUserControl() => this;
+
+    public string NeedProjects => "RapidDeskToolkit\nRapidDeskToolkit.UIPackage";
+
+    public string StylesName => "<Styles>\n\t...\n\t<uiPackage:Generic />\n\t...\n</Styles>";
 }
